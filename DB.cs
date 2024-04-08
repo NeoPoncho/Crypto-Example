9234da66274e03173ab0c7e3b8033983096dd462
Pedro Teixeira <93651985+NeoPoncho@users.noreply.github.com>
2024-04-08 16:56:54 +0100

    Update DB.cs

--- DB.cs
+++ DB.cs
@@ -1,19 +1,8 @@
-﻿namespace DeliveryProject.Models
+﻿namespace Application.Models
 {
     public static class DB
     {
         private const string encryptionPassphrase = "UIr<Fy+{Z#fcL:xlaNP_)P2@zgKh#M";
-
-        private const string LocalDBfilename = "DatabaseDelivery.db3";
-
-        public static string LocalDatabasePath
-        {
-            get
-            {
-                var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-                return Path.Combine(folderPath, LocalDBfilename);
-            }
-        }
 
         public static string EncryptionKey
         {

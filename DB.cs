eabda8ae4acfedaa79e5843b44ae614c64786028
Pedro Teixeira <93651985+NeoPoncho@users.noreply.github.com>
2024-04-08 16:56:19 +0100

    Add files via upload

---
+++ DB.cs
@@ -0,0 +1,26 @@
+﻿namespace DeliveryProject.Models
+{
+    public static class DB
+    {
+        private const string encryptionPassphrase = "UIr<Fy+{Z#fcL:xlaNP_)P2@zgKh#M";
+
+        private const string LocalDBfilename = "DatabaseDelivery.db3";
+
+        public static string LocalDatabasePath
+        {
+            get
+            {
+                var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                return Path.Combine(folderPath, LocalDBfilename);
+            }
+        }
+
+        public static string EncryptionKey
+        {
+            get
+            {
+                return Path.Combine(encryptionPassphrase);
+            }
+        }
+    }
+}
